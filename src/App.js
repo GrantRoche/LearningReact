12fe02ed33fad65f688c41e1b1cf3c4553f1dd2c
Grant.Mesa <Grant.Mesa@ertechnologies.com>
2019-05-10 18:44:16 +0800

    Update: Add data and API integration

--- src/App.js
+++ src/App.js
@@ -1,45 +1,37 @@
 import React, { Component } from 'react';
 import Table from './Table';
+import Form from './Form';
+import Api from './Api';
 
 
 class App extends Component {
 	
-		state = {
-			characters: [
-				{
-					name: 'Charlie',
-					job: 'Janitor'
-				},
-				{
-					name: 'Mac',
-					job: 'Bouncer'
-				},
-				{
-					name: 'Dee',
-					job: 'Aspiring actress'
-				},
-				{
-					name: 'Dennis',
-					job: 'Bartender'
-				}
-			]
-		}
+	state = {
+		characters: []
+	}
 
-		removeCharacter = index => {
-			const { characters } = this.state
-		
-			this.setState({
-				characters: characters.filter((character, i) => { 
-					return i !== index
-				})
+	removeCharacter = index => {
+		const { characters } = this.state
+	
+		this.setState({
+			characters: characters.filter((character, i) => { 
+				return i !== index
 			})
-		}
-		
+		})
+	}
+
+	handleSubmit = character => {
+		this.setState({ characters: [...this.state.characters, character] })
+	}
+
 	render() {
 		const { characters } = this.state
 		return (			
 			<div className="container">
-				<Table characterData={characters} removeCharacter={this.removeCharacter} />
+				<Table characterData = {characters} removeCharacter = {this.removeCharacter} />
+				<Form handleSubmit = {this.handleSubmit} />
+				<br />
+				<Api />
 			</div>
 		)
 	}
